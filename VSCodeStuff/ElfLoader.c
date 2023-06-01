5c6170c050e208b06392918210ea99fde4bc4bed
DanielJunsangCho <junc13579@gmail.com>
2023-06-01 10:40:11 -0400

    some deletes

--- VSCodeStuff/ElfLoader.c
+++ VSCodeStuff/ElfLoader.c
@@ -10,7 +10,7 @@
 
 
 int main(){
-    FILE* fp = fopen("/home/danielcho/Desktop/NSF-REU/ELFFileManipulator/VSCodeStuff/example.c", "rb");
+    FILE* fp = fopen("/home/danielcho/Desktop/NSF-REU/ELFFileManipulator/VSCodeStuff/example", "rb");
     if(fp == NULL)
     {
         printf("failed to load\n");
@@ -27,29 +27,6 @@ int main(){
     printf("%d\n",hdr.e_version);
 
     Elf64_Phdr p[hdr.e_phnum];
-
-
-
-
-    printf("ELF e_ident padding: \n");
-    for (int i = 9; i < 16; i++)
-    {
-        printf("%d\n", hdr.e_ident[i]);
-    }
-
-    hdr.e_ident[9] = 99; //changing the first byte of padding in e_ident in ELF header, original is 115
-
-    printf("ELF e_ident padding after: \n");
-    for (int i = 9; i < 16; i++)
-    {
-        printf("%d\n", hdr.e_ident[i]);
-    }
-
-    rewind(fp);
-    fwrite(&hdr, sizeof(hdr), 1, fp);
-    fclose(fp);
-
-    
 
 
 
