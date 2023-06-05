f4387709cf039085cea4787f8a7af0463dbb6f82
DanielJunsangCho <junc13579@gmail.com>
2023-06-05 15:07:29 -0400

    Yay

--- VSCodeStuff/copy.c
+++ VSCodeStuff/copy.c
@@ -18,26 +18,34 @@
          exit(1);
       }
 
-      printf("%d\n",hdr.e_version);
+      // printf("%d\n",hdr.e_version);
 
-      Elf64_Phdr p[hdr.e_phnum];
-
-
+      // Elf64_Phdr p[hdr.e_phnum];
 
 
-      printf("ELF e_ident padding: \n");
-      for (int i = 9; i < 16; i++)
-      {
-         printf("%d\n", hdr.e_ident[i]);
-      }
 
-      hdr.e_ident[9] = 99; //changing the first byte of padding in e_ident in ELF header, original is 115
 
-      printf("ELF e_ident padding after: \n");
-      for (int i = 9; i < 16; i++)
-      {
-         printf("%d\n", hdr.e_ident[i]);
-      }
+      // printf("ELF e_ident padding: \n");
+      // for (int i = 9; i < 16; i++)
+      // {
+      //    printf("%d\n", hdr.e_ident[i]);
+      // }
+
+      // for (int i = 9; i < 16; i++)
+      // {
+      //    hdr.e_ident[i] = 0; //changing the first byte of padding in e_ident in ELF header, original is 0
+      // }
+
+      // // hdr.e_ident[9] = 1;
+
+      // printf("ELF e_ident padding after: \n");
+      // for (int i = 9; i < 16; i++)
+      // {
+      //    printf("%d\n", hdr.e_ident[i]);
+      // }
+
+      printf("ELF e_ident size before: \n");
+
 
       rewind(fp);
       fwrite(&hdr, sizeof(hdr), 1, fp);
