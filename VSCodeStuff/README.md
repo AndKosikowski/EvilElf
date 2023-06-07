09581543842114b9e4c86b68b8eef4c0ef5819b2
Andrew Kosikowski <andkosikowski@gmail.com>
2023-06-07 17:38:41 -0400

    ADded section to ELF file without changing function

---
+++ VSCodeStuff/README.md
@@ -0,0 +1,10 @@
+#Repository for Editing ELF Files
+
+### By Andrew Kosikowski and Daniel Jun Cho
+
+Useful command for comparing hex in files:
+cmp -l example ModifiedElfOutput/example | gawk '{printf "%08X %02X %02X\n", $1, strtonum(0$2), strtonum(0$3)}'
+
+Valgrind because you are using C:
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./add_elf_section example
+
--- VSCodeStuff/add_elf_section.c
+++ VSCodeStuff/add_elf_section.c
@@ -23,19 +23,24 @@ int main(int argc, char** argv){
         }
     }
 
+    printf("Last Section: %x - %lx",final_offset, final_offset + manager->s_hdr[final_section].sh_size);
+
     //https://stackoverflow.com/questions/2022179/c-quick-calculation-of-next-multiple-of-4
     //Adding section at next multiple of 4/8/16/32/64 might be needed later
     
-    void* temp = realloc(manager->s_hdr, sizeof(Elf64_Shdr) * (manager->e_hdr.e_shnum + 1));
-    if(temp == NULL){
+    manager->s_hdr = realloc(manager->s_hdr, sizeof(Elf64_Shdr) * (manager->e_hdr.e_shnum + 1));
+    if(manager->s_hdr == NULL){
         printf("Realloc space lacking for realloc\n");
         return 1;
     }
 
-    Elf64_Shdr* new_section = &(manager->s_hdr[manager->e_hdr.e_shnum]);
+    Elf64_Shdr new_section = manager->s_hdr[manager->e_hdr.e_shnum];//Point to new last section
+    memcpy(&new_section, &(manager->s_hdr[manager->e_hdr.e_shnum-1]), sizeof(Elf64_Shdr)); //Copy old last section into it
     manager->e_hdr.e_shnum += 1;
-    new_section->sh_offset = final_offset + new_section->sh_size;
-    new_section->sh_size = 1024;    
+    new_section.sh_offset = final_offset + new_section.sh_size;
+    new_section.sh_size = 1024*1024;  
+
+    manager->e_hdr.e_shoff += new_section.sh_size;
 
     write_elf64_file(manager, argv[1]);
 
@@ -43,22 +48,37 @@ int main(int argc, char** argv){
     int size = get_file_name_size_from_path(argv[1]);
     char output_path[19+size];
     strcpy(output_path, folder);
-
     strcat(output_path, argv[1]+strlen(argv[1])-size);
 
-
-    // printf("%x\n",new_section->sh_offset);
-
     FILE* fp = fopen(output_path, "r+b");
-    fseek(fp, new_section->sh_offset-1,SEEK_SET);
+    fseek(fp, new_section.sh_offset,SEEK_SET);
 
-    for(int i = new_section->sh_offset; i < new_section->sh_offset + new_section->sh_size-4; i++){
-        // printf("Do I get here\n");
-        int j = i;
+    for(int i = new_section.sh_offset; i < new_section.sh_offset + new_section.sh_size; i++){
+        uint8_t j = 4;
         fwrite(&j, sizeof(j), 1, fp);
     }
 
+    fseek(fp,manager->e_hdr.e_shstrndx, SEEK_SET);
+    int num_strings = manager->e_hdr.e_shnum;
+    int i = 0;
+    while(i < num_strings){
+        char c = fgetc(fp);
+        if ( c == '\0'){
+            i++;
+            printf("\n");
+        }else{
+            printf("%c",c);
+        }
+        
+    }
+    // for(int i = 0; i < manager->e_hdr.e_shnum; i++){
+    //     char buff[1024];
+    //     fgets(buff, 1024, fp);
+    //     printf("%s\n",buff);
+    //     // fseek(fp, manager->s_hdr[i].sh_name, SEEK_CUR);
+    // }
 
     free_manager64(manager);
+    fclose(fp);
     return 0;
 }
